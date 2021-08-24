532377e71cdc76d8db4049457880e7fe1bdcb289
Mike Frager <mfrager@gmail.com>
2021-08-24 10:42:03 -0700

    First successful compilation

--- programs/token-agent/src/lib.rs
+++ programs/token-agent/src/lib.rs
@@ -1,18 +1,18 @@
-use uuid::Uuid;
-use std::{ mem::size_of, io::Cursor };
-use bytemuck::{ Pod, Zeroable };
-use byte_slice_cast::*;
+//use uuid::Uuid;
+//use std::{ mem::size_of, io::Cursor };
+//use bytemuck::{ Pod, Zeroable };
+//use byte_slice_cast::*;
 use num_enum::TryFromPrimitive;
 use anchor_lang::prelude::*;
-use anchor_spl::token::{ self, Transfer, TokenAccount, Mint, Approve };
+use anchor_spl::token::{ self, Transfer, TokenAccount, Approve };
 use solana_program::{
     program::{ invoke_signed },
     account_info::AccountInfo,
     system_instruction,
 };
 
-extern crate slab_alloc;
-use slab_alloc::{ SlabPageAlloc, CritMapHeader, LeafNode, AnyNode, CritMap, SlabVec };
+//extern crate slab_alloc;
+//use slab_alloc::{ SlabPageAlloc, CritMapHeader, LeafNode, AnyNode, CritMap, SlabVec };
 
 #[repr(u8)]
 #[derive(PartialEq, Debug, Eq, Copy, Clone)]
@@ -36,11 +36,13 @@ pub enum Status { // Status types
 mod token_agent {
     use super::*;
 
-    pub fn create_subscription(ctx: Context<CreateSubscr>) -> ProgramResult {
+    pub fn create_subscription(ctx: Context<CreateSubscr>,
+//        bool link_token
+    ) -> ProgramResult {
         Ok(())
     }
 
-    pub fn update_subscription() -> ProgramResult {
+/*    pub fn update_subscription() -> ProgramResult {
         Ok(())
     }
 
@@ -62,7 +64,7 @@ mod token_agent {
 
     pub fn delegated_transfer() -> ProgramResult {
         Ok(())
-    }
+    } */
 }
 
 #[derive(Accounts)]
@@ -81,9 +83,19 @@ pub struct CreateSubscr<'info> {
     pub rebill_data: AccountInfo<'info>,
 }
 
+#[repr(u8)]
+#[derive(PartialEq, Debug, Eq, Copy, Clone)]
+pub enum SubscriptionPeriod {
+    Daily,
+    Weekly,
+    Monthly,
+    Quarterly,
+    Yearly,
+}
+
 #[account]
 pub struct SubscrData {
-    pub data_type: AccountDataType,     // AccountDataType to prevent mixing and matching of data
+    pub data_type: u8,                  // AccountDataType to prevent mixing and matching of data
     //pub approval_program: Pubkey,       // The address of the network authority program that signs approvals
     pub merchant_key: Pubkey,           // The merchant account that receives subscription payments
     //pub merchant_approval: Pubkey,      // The merchant approval record from the network authority
@@ -92,10 +104,16 @@ pub struct SubscrData {
     pub token_mint: Pubkey,             // The token mint to pay for the subscription
     pub token_account: Pubkey,          // The token account to pay for the subscription
     pub rebill_data: Pubkey,            // The rebill data account to track subscription rebills and prevent duplicates
+    // Subscription details below
     pub rebill_events: u32,             // Count of rebill events
+    pub rebill_max: u32,                // Maximum number of times to rebill (0 = unlimited)
     pub not_valid_before: u64,          // UTC timestamp before which no subscription processing can occur
     pub not_valid_after: u64,           // UTC timestamp after which no subscription processing can occur
-    // TODO: Add subscription details
+    pub subscr_uuid: u128,              // Subscription UUID
+    pub period: u8,                     // Subscription rebill period
+    pub budget: u64,                    // Subscription budget (maximum amount, not necessarily the amount that will be billed which could be less)
+    pub pause_enabled: bool,            // Subscription able to be paused
+    pub paused: bool,                   // Subscription is paused
 }
 
 // TODO: Merchant approval
@@ -104,7 +122,7 @@ pub struct SubscrData {
 
 #[account]
 pub struct AssignRebillManager {
-    pub data_type: AccountDataType,     // AccountDataType to prevent mixing and matching of data
+    pub data_type: u8,                  // AccountDataType to prevent mixing and matching of data
     pub subscr_data: Pubkey,            // The subscription data for this assignment
     pub rebill_approval: Pubkey,        // The rebill manager approval from the network authority
     pub manager_key: Pubkey,            // The rebill manager account being assigned
@@ -112,7 +130,7 @@ pub struct AssignRebillManager {
 
 #[account]
 pub struct TokenAllowance {
-    pub data_type: AccountDataType,     // AccountDataType to prevent mixing and matching of data
+    pub data_type: u8,                  // AccountDataType to prevent mixing and matching of data
     //pub abort_authority: Pubkey,        // The abort authority from the network authority to abort in case of hacks
     pub user_key: Pubkey,               // The user that owns the tokens
     pub delegate_key: Pubkey,           // The delegate granted an allowance of tokens to transfer
